8bfa11de2ba24ead7b3e0a578b32d28a88db1215
PauFusco <99730051+PauFusco@users.noreply.github.com>
2025-06-03 18:23:32 +0200

    First dialogue
    Added first dialogue to pres 1

--- Assets/Resources/Scripts/Characters/Pres1_Manager.cs
+++ Assets/Resources/Scripts/Characters/Pres1_Manager.cs
@@ -10,12 +10,17 @@ public class Pres1_Manager : MonoBehaviour, IInteractable
 
     public bool canInteract { get; set; } = false;
 
+    [SerializeField] NPCConversation dialogue1;
     [SerializeField] NPCConversation dialogue2A;
 
+    [SerializeField] QuestSO defaultQuest;
     [SerializeField] QuestSO parlaPresApallissat;
 
     public void Interact(Interactor interactor)
     {
+        if (QuestManager.Singleton.activeQuests.Contains(defaultQuest))
+            ConversationManager.Instance.StartConversation(dialogue1);
+
         if (QuestManager.Singleton.activeQuests.Contains(parlaPresApallissat))
             ConversationManager.Instance.StartConversation(dialogue2A);
     }
