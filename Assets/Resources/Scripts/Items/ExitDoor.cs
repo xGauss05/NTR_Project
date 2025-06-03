5c2bf0dc7cc795dd30f7f790d079bfef50dd5117
Ethanm-0371 <ethanm0371@gmail.com>
2025-06-03 21:46:46 +0200

    Created Exit Doors.

---
+++ Assets/Resources/Scripts/Items/ExitDoor.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ExitDoor : MonoBehaviour, IInteractable
+{
+    [SerializeField] string interactText;
+    public string interactableText => interactText;
+
+    [SerializeField] private ItemSO itemToOpen;
+
+    public bool canInteract { get; set; } = false;
+
+    public void Interact(Interactor interactor)
+    {
+        if (ItemManager.Singleton.CheckItemInPlayer(itemToOpen))
+        {
+            Transitioner.Singleton.FadeToBlack("0_MainMenu");
+        }
+        else
+        {
+            Debug.Log("Bobo you dont have object");
+        }
+    }
+}
