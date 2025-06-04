20c4ff299a2220ffc73a152b23940f64322e2c90
Ethanm-0371 <99729919+Ethanm-0371@users.noreply.github.com>
2025-06-04 10:13:03 +0200

    Fixed pick up items from start.

--- Assets/Resources/Scripts/Items/ItemInteractable.cs
+++ Assets/Resources/Scripts/Items/ItemInteractable.cs
@@ -5,7 +5,7 @@ public class ItemInteractable : MonoBehaviour, IInteractable
     [SerializeField] private ItemSO item;
 
     public string interactableText { get; private set; }
-    public bool canInteract { get; set; } = true;
+    public bool canInteract { get; set; } = false;
 
     [SerializeField] Sofia_Manager sofiaManager;
     [SerializeField] private QuestSO sofiaQuest;
